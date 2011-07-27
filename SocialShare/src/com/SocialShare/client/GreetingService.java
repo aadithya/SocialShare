ff2e27f73a42f6bceb68d76abf1bf39dd1d04b27
aadithya <aadithya.c@gmail.com>
2011-07-28 02:15:38 +0530

    First Commit ___ 2

--- SocialShare/src/com/SocialShare/client/GreetingService.java
+++ SocialShare/src/com/SocialShare/client/GreetingService.java
@@ -6,6 +6,8 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 /**
  * The client side stub for the RPC service.
  */
+/*Aadi: I just edited this */
+
 @RemoteServiceRelativePath("greet")
 public interface GreetingService extends RemoteService {
 	String greetServer(String name);
