d42c11101613652ec1a3950dfec547064c8b3707
vijeth-dinesha <vijeth.d@gmail.com>
2011-07-28 02:21:38 +0530

    Testing git

--- SocialShare/src/com/SocialShare/server/GreetingServiceImpl.java
+++ SocialShare/src/com/SocialShare/server/GreetingServiceImpl.java
@@ -13,7 +13,7 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 	public String greetServer(String input) {
 		String serverInfo = getServletContext().getServerInfo();
 		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
+		return "Welcome, " + input + "!<br><br>I am running " + serverInfo
 				+ ".<br><br>It looks like you are using:<br>" + userAgent;
 	}
 }
